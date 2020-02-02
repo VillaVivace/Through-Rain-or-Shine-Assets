e45996d0e36fc9e9f29de478fd4c4888d0cab915
Dominic Villa <cillavivacemusic@gmail.com>
2020-02-02 13:38:02 -0800

    Battle System Nearly Completed
    I got soooo much done!!!

---
+++ Scripts/GhostConvo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostConvo : MonoBehaviour
+{
+    public DialogueTrigger dt;
+
+    void Start() {
+        dt = GetComponent<DialogueTrigger>();
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log(other);
+        dt.TriggerDialogue();
+    }
+}
---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    public string state = "normal";
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update() {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() {
+        switch(state) {
+            case "normal":
+                rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+                break;
+            case "talking":
+                break;
+            default:
+                break;
+        }
+    }
+}
